750966be9cda20be7d612cfac3feaf47b1ff5371
Mykhailo Stadnyk <mikhus@gmail.com>
2016-08-17 18:29:23 +0300

    vendorize() functional unit-test checks added

--- test/spec/vendorize.spec.js
+++ test/spec/vendorize.spec.js
@@ -5,4 +5,11 @@ describe('vendorize', () => {
     it('should be a function', () => {
         expect(vendorize).is.a('function');
     });
+    it('should return null on non-existing properties request', () => {
+        expect(vendorize('ndkwjn3r0932##$#435gdej')).is.a('null');
+    });
+    it('should return proper reference on existing property', () => {
+        expect(vendorize('requestAnimationFrame')).is.a('function');
+        expect(vendorize('requestAnimationFrame')).match(/\[native code\]/);
+    });
 });
