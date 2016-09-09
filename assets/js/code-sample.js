591dd7e53166e0194d34a134c040d65e8fb51f4e
Mykhailo Stadnyk <mikhus@gmail.com>
2016-09-10 01:06:21 +0300

    Code box for examples implemented

---
+++ assets/js/code-sample.js
@@ -0,0 +1,260 @@
+(function() {
+    'use strict';
+
+    var styles = $('<style>\
+        .tabs-container {\
+            margin: 0 auto;\
+            width: 50%;\
+            border: 25px solid rgba(255,230,106,1);\
+            background: #fff;\
+            overflow: hidden;\
+        }\
+        .tabs-container .icon-circle-with-cross {\
+            float: right;\
+            font-size: 24px;\
+            cursor: pointer;\
+            margin-right: 5px;\
+            color:#999;\
+        }\
+        .tabs-container .icon-circle-with-cross:hover {\
+            color:#222\
+        }\
+        .tabs-container input {\
+            visibility: hidden;\
+        }\
+        .tabs-container label {\
+            background: #fff;\
+            color: #888;\
+            cursor: pointer;\
+            display: block;\
+            float: left;\
+            font-size: 1em;\
+            height: 2.5em;\
+            line-height: 2.5em;\
+            padding: 0 1.5em;\
+            text-align: center;\
+        }\
+        .tabs-container input:hover + label {\
+            background: #eee;\
+            color: #222;\
+        }\
+        .tabs-container input:checked + label {\
+            background: #eee;\
+            color: #444;\
+            position: relative;\
+            z-index: 6;\
+        }\
+        .tabs-content {\
+            min-height: 20em;\
+            position: relative;\
+            width: 100%;\
+            z-index: 5;\
+        }\
+        .tabs-content pre {\
+            width: 100%;\
+            border: 0;\
+            opacity: 0;\
+            padding: 10px 15px;\
+            position: absolute;\
+            z-index: -100;\
+            overflow: auto;\
+            height: 100%;\
+        }\
+        .tabs-container input.tab-html:checked ~ .tabs-content .tab-html-content,\
+        .tabs-container input.tab-js:checked ~ .tabs-content .tab-js-content {\
+            opacity: 1;\
+            z-index: 100;\
+        }\
+        input.visible {\
+            visibility: visible !important;\
+        }\
+        </style>');
+    var body = $('body');
+    var shade = $('#shade-cover');
+
+    body.append(styles);
+
+    if (!shade.length) {
+        shade = $('<div id="shade-cover">').css({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            zIndex: 1,
+            background: 'rgba(36,101,192, .85)'
+        }).hide();
+        body.append(shade);
+    }
+
+    shade.pos = function() {
+        var html = document.documentElement;
+        var height = Math.max(body[0].scrollHeight, body[0].offsetHeight,
+            html.clientHeight, html.scrollHeight, html.offsetHeight);
+
+        shade.css({
+            width: body.width() + 'px',
+            height: height + 'px'
+        });
+    };
+
+    function toOption(attr) {
+        return attr.split('-').map(function(part, i) {
+            return i ? (i === 1 ?
+                part.toLowerCase() :
+                part.charAt(0).toUpperCase() + part.substr(1).toLowerCase()) :
+                '';
+        }).join('');
+    }
+
+    function parse(value) {
+        if (value === 'true') return true;
+        if (value === 'false') return false;
+        if (value === 'undefined') return undefined;
+        if (value === 'null') return null;
+        if (/^[\w\d\s]+(?:,[\w\d\s]+)+$/.test(value)) {
+            return value.split(',');
+        }
+        try { return JSON.parse(value); } catch(e) {}
+        return value;
+    }
+
+    function toJs(src) {
+        var code = 'var gauge = ';
+        var type = src.getAttribute('data-type');
+        var keys = Object.keys(src.attributes);
+        var s = keys.length;
+
+        if (type === 'linear-gauge') {
+            code += 'new LinearGauge({\n';
+        }
+
+        else if (type === 'radial-gauge') {
+            code += 'new RadialGauge({\n';
+        }
+
+        else {
+            return '';
+        }
+
+        code += '    renderTo: \'canvas-id\',\n';
+
+
+        keys.forEach(function (i) {
+            var attr = src.attributes[i];
+
+            if (attr.name.substr(0, 5) === 'data-' && attr.name !== 'data-type') {
+                code += '    ' + toOption(attr.name) + ':' +
+                    JSON.stringify(parse(attr.value), null, 4).split(/\r?\n/).map(function(line, i) {
+                        return (i ? '    ' : '') + line;
+                    }).join('\n') + (i == s - 1 ? '' : ',') + '\n';
+            }
+        });
+
+        code += '}).draw();\n';
+
+        return code;
+    }
+
+    $('.example').each(function(id) {
+        var tabs = $('\
+        <div id="tabs-container' + id + '" class="tabs-container">\
+            <input id="tab-html' + id + '" type="radio" class="tab-html" name="tab-group">\
+            <label for="tab-html' + id + '">HTML</label>\
+            <input id="tab-js' + id + '" type="radio" class="tab-js" name="tab-group">\
+            <label for="tab-js' + id + '">JavaScript</label>\
+            <span class="icon-circle-with-cross"></span>\
+            <div class="tabs-content">\
+                <pre class="tab-html-content prettyprint lang-html"></pre>\
+                <pre class="tab-js-content prettyprint lang-javascript"> </pre>\
+            </div>\
+        </div>');
+
+        var src = $(this);
+        var code = src.html().replace(/^\s+|\s+$/g, '');
+
+        var bodyOverflow = body[0].style.overflow;
+        var boxHtml = tabs.find('.tab-html-content');
+        var boxJs = tabs.find('.tab-js-content');
+
+        if (code.length > 80) {
+            code = code.replace(/(\w+=".*?")\s/g, '$1\n    ')
+                .replace(/(\w+=".*?")>/g, '$1\n>');
+        }
+
+        body.append(tabs);
+        boxHtml.text(code);
+        boxJs.text(toJs($(this).find('canvas')[0]));
+
+        tabs.css({
+            position: 'absolute',
+            display: 'none',
+            zIndex: 2
+        });
+
+        tabs.pos = function() {
+            tabs.css({
+                left: ((window.innerWidth - tabs.width()) / 2) + 'px',
+                top: ((window.innerHeight - tabs.height()) / 2) + body[0].scrollTop + 'px',
+            });
+        };
+
+        tabs.show = function() {
+            tabs.find('#tab-html' + id).click();
+            tabs.css({
+                visibility: 'hidden',
+                display: 'block'
+            });
+
+            body.css({ overflow: 'hidden' });
+
+            if (tabs[0].scrollHeight > tabs[0].offsetHeight) {
+                tabs.css({
+                    width: tabs[0].offsetWidth + 20 + 'px'
+                });
+            }
+
+            tabs.pos();
+            shade.pos();
+
+            tabs.css({ visibility: 'visible' });
+            shade.css({ display: 'block' });
+            shade.lockTo = tabs;
+        };
+
+        tabs.hide = function() {
+            tabs.css({ display: 'none' });
+
+            if (shade.lockTo === tabs) {
+                shade.css({ display: 'none' });
+                delete shade.lockTo;
+                body.css({ overflow: bodyOverflow });
+            }
+        };
+
+        tabs.find('.icon-circle-with-cross').on('click', tabs.hide);
+
+        tabs.toggle = function() {
+            tabs[0].style.display === 'none' ? tabs.show() : tabs.hide();
+        };
+
+        body.on('mousedown', function(e) {
+            var parent = e.target;
+
+            while (parent) {
+                if (parent === tabs[0] || parent === src[0]) {
+                    return;
+                }
+
+                parent = parent.parentNode;
+            }
+
+            tabs.hide();
+        });
+
+        src.on('click', tabs.toggle);
+
+        $(window).on('resize', function() {
+            tabs.pos();
+            shade.pos();
+        });
+    });
+}());
--- pages/documentation/examples.md
+++ pages/documentation/examples.md
@@ -7,22 +7,37 @@ permalink: "/documentation/examples/"
 breadcrumb: true
 header: false
 ---
+<script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
+<script src="/assets/js/jquery-3.1.0.min.js"></script>
 <script src="/assets/js/gauge.min.js"></script>
 <style>
-.example { min-height: 200px; }
+.example { min-height: 200px }
+.example canvas { cursor: pointer; cursor:hand }
 </style>
+<script>
+function showCode(canvas) {
+    let code = canvas.parentNode.getElementsByTagName('pre')[0];
+    code.style.top = (window.innerHeight - code.offsetHeight) / 2 + 'px';
+    code.style.left = (window.innerWidth - code.offsetWidth) / 2 + 'px';
+    code.style.visibility = 'visible';
+}
+</script>
 
 Canvas Gauges are highly configurable library. So you may use your fantasy to draw different gauges of different look and feel, displaying different kind of information. Here we simply tried to show you what it can be, when simply changing default gauge options.
 
-### Zero Configuration Gauges
+## Zero Configuration Gauges
 
 Minimum declaration!
 
 {% include examples/zero-configuration.html %}
 
-### Radial Gauges as Compass
+## Radial Gauges as Compass
 
 Compass wanted?
 
 {% include examples/compass.html %}
 
+
+## Other Custom Views
+
+<script src="/assets/js/code-sample.js"></script>
