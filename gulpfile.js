906d7fa970250a7b788d6c921926e9a6c4b4e515
Mykhailo Stadnyk <mikhus@gmail.com>
2016-09-15 16:35:54 +0300

    Watch task implemented for js bundling

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,17 @@ const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', done => {
-    gulp.src([
-            'assets/js/javascript.min.js',
-            'assets/js/modernizr.js',
-            'assets/js/gauge.min.js',
-            'assets/js/slick.min.js',
-            'assets/js/prettify.js',
-            'assets/js/code-sample.js'
-        ])
+let files = [
+    'assets/js/javascript.min.js',
+    'assets/js/modernizr.js',
+    'assets/js/gauge.min.js',
+    'assets/js/slick.min.js',
+    'assets/js/prettify.js',
+    'assets/js/code-sample.js'
+];
+
+gulp.task('build', done => {
+    gulp.src(files)
         .pipe(sourcemaps.init())
         .pipe(concat('bundle.js'))
         .pipe(uglify())
@@ -19,3 +21,9 @@ gulp.task('default', done => {
         .pipe(gulp.dest('assets/js'))
         .on('end', () => done());
 });
+
+gulp.task('watch', ['build'], () => {
+    gulp.watch(files, () => gulp.start('build'));
+});
+
+gulp.task('default', ['build']);
